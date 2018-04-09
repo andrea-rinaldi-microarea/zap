f412573a76a221fd7a8337c77d1a89f59fa46eb5
andrea-rinaldi-microarea <andrea.rinaldi@microarea.it>
2018-04-09 17:04:15 +0200

    no empty strings for numbers in XML

--- src/app/model/rule.model.ts
+++ src/app/model/rule.model.ts
@@ -12,8 +12,18 @@ export const AllRules: string[] = [
 
 export class Copy implements Rule {
     apply(source: string, target: Column): string {
-        if (target.type == Type.Money || target.type == Type.Double) {//, Type.Float, Type.Percent, Type.Quantity: {
+        if (target.type == Type.Money || target.type == Type.Double || target.type == Type.Float || target.type == Type.Quantity) {
+            if (source === "") {
+                return ("0.0"); // avoid leaving empty strings for numbers in XML, console import fails otherwise
+            } else {
                 return source.replace(',','.');    
+            }
+        } else if (target.type == Type.Integer || target.type == Type.Long ) {
+            if (source === "") {
+                return ("0"); // avoid leaving empty strings for numbers in XML, console import fails otherwise
+            } else {
+                return source;    
+            }
         } else {
             return source;    
         }
